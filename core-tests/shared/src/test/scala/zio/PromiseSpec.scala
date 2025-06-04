c6cfc762dde2fffc659d350406497f6ae2e4db9d
Saturn225 <101260782+Saturn225@users.noreply.github.com>
2025-06-04 23:20:38 +0530

    Update PromiseSpec.scala

--- core-tests/shared/src/test/scala/zio/PromiseSpec.scala
+++ core-tests/shared/src/test/scala/zio/PromiseSpec.scala
@@ -134,7 +134,7 @@ object PromiseSpec extends ZIOBaseSpec {
         _      <- p.complete(Exit.unit)
         _      <- ZIO.foreach(fibers)(_.await)
       } yield assertCompletes
-    }@@ nonFlaky(20),
+    }@@ nonFlaky(100),
     suite("State")(
       suite("add")(
         test("stack safety") {
